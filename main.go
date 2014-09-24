259ea880d5948d1bd6d5f70341e9cf21124af59e
pavel-paulau <pavel.paulau@gmail.com>
2014-09-23 20:43:42 -0700

    Show full URL when starting

--- main.go
+++ main.go
@@ -137,6 +137,6 @@ func main() {
 	r.HandleFunc("/{db}/{source}/{metric}/linechart", GetLineChart).Methods("GET")
 	http.Handle("/", r)
 
-	fmt.Println("\n\t:-:-: perfkeeper :-:-:\t\t\tserving 0.0.0.0:8080\n")
+	fmt.Println("\n\t:-:-: perfkeeper :-:-:\t\t\tserving http://0.0.0.0:8080/\n")
 	log.Fatal(http.ListenAndServe("0.0.0.0:8080", Log(http.DefaultServeMux)))
 }
