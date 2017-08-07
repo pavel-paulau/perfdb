73e25672eb013460e6d1cfe4c15f3a3e5763b181
Pavel Paulau <pavel.paulau@gmail.com>
2017-08-07 13:33:54 -0700

    Improve test coverage

--- time_test.go
+++ time_test.go
@@ -2,20 +2,40 @@ package main
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestTimestampParser(t *testing.T) {
-	tsNano := parseTimestamp("1411534805453497432")
-	assert.Equal(t, tsNano, int64(1411534805453497432), "Invalid ns")
+	timestamp := parseTimestamp("1411534805453497432")
+	assert.Equal(t, timestamp, int64(1411534805453497432), "Invalid ns")
 
-	tsNano = parseTimestamp("1411534805453497")
-	assert.Equal(t, tsNano, int64(1411534805453497000), "Invalid us")
+	timestamp = parseTimestamp("1411534805453497")
+	assert.Equal(t, timestamp, int64(1411534805453497000), "Invalid us")
 
-	tsNano = parseTimestamp("1411534805453")
-	assert.Equal(t, tsNano, int64(1411534805453000000), "Invalid ms")
+	timestamp = parseTimestamp("1411534805453")
+	assert.Equal(t, timestamp, int64(1411534805453000000), "Invalid ms")
 
-	tsNano = parseTimestamp("1411534805")
-	assert.Equal(t, tsNano, int64(1411534805000000000), "Invalid s")
+	timestamp = parseTimestamp("1411534805")
+	assert.Equal(t, timestamp, int64(1411534805000000000), "Invalid s")
+}
+
+func TestBadTimestampParser(t *testing.T) {
+	timeNow := time.Now().UnixNano()
+
+	timestamp := parseTimestamp("1411534805.453")
+
+	if timestamp < timeNow || timestamp > (timeNow+1e9) {
+		t.Fatalf("Bad (not current) time: %v, expected ~%v", timestamp, timeNow)
+	}
+}
+
+func TestSmallTimestampParser(t *testing.T) {
+	timeNow := time.Now().UnixNano()
+
+	timestamp := parseTimestamp("123456")
+	if timestamp < timeNow || timestamp > (timeNow+1e9) {
+		t.Fatalf("Bad (not current) time: %v, expected ~%v", timestamp, timeNow)
+	}
 }
