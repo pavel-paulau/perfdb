a30511f834b8a31b12b32b0163ff9aff64330ee2
Pavel Paulau <pavel.paulau@gmail.com>
2017-08-07 10:50:58 -0700

    Unnest code blocks in parseTimestamp

--- time.go
+++ time.go
@@ -6,20 +6,22 @@ import (
 )
 
 func parseTimestamp(ts string) int64 {
-	if tsInt, err := strconv.ParseInt(ts, 10, 64); err == nil {
-		switch {
-		case tsInt > 1e18: // nanosecond timestamps
-			return time.Unix(tsInt/1e9, tsInt%1e9).UnixNano()
-		case tsInt > 1e15: // microseconds timestamps
-			return time.Unix(tsInt/1e6, (tsInt*1e3)%1e9).UnixNano()
-		case tsInt > 1e12: // millisecond timestamps
-			return time.Unix(tsInt/1e3, (tsInt*1e6)%1e9).UnixNano()
-		case tsInt > 1e9: // second timestamps
-			return time.Unix(tsInt, 0).UnixNano()
-		}
-	} else {
+	tsInt, err := strconv.ParseInt(ts, 10, 64)
+	if err != nil {
 		logger.Warning("Invalid timestamp, using current time instead.")
 		return time.Now().UnixNano()
 	}
-	panic("Unreachable")
+
+	switch {
+	case tsInt > 1e18: // nanosecond timestamps
+		return time.Unix(tsInt/1e9, tsInt%1e9).UnixNano()
+	case tsInt > 1e15: // microseconds timestamps
+		return time.Unix(tsInt/1e6, (tsInt*1e3)%1e9).UnixNano()
+	case tsInt > 1e12: // millisecond timestamps
+		return time.Unix(tsInt/1e3, (tsInt*1e6)%1e9).UnixNano()
+	case tsInt > 1e9: // second timestamps
+		return time.Unix(tsInt, 0).UnixNano()
+	default:
+		return time.Now().UnixNano()
+	}
 }
