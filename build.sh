d2efff892759db40c4782ca94f225e5746c2bb84
Pavel Paulau <pavel.paulau@gmail.com>
2017-08-09 10:38:57 -0700

    Create builds in the same directory

--- build.sh
+++ build.sh
@@ -8,9 +8,7 @@ for os in "windows" "linux" "darwin"; do
         ext=".exe"
     fi
 
-    mkdir -p build/${os}
     for arch in "386" "amd64"; do
-        mkdir build/${os}/${arch}
-        GOOS=${os} GOARCH=${arch} go build -a -tags netgo --ldflags '-extldflags "-lm -lstdc++ -static"' -o build/${os}/${arch}/perfdb${ext}
+        CGO_ENABLED=0 GOOS=${os} GOARCH=${arch} go build -a -tags netgo --ldflags '-s' -o build/perfdb.${os}-${arch}${ext}
     done
 done
